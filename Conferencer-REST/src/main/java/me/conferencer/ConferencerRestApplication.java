99163a31dcdd44f401df8d7a2010ee820c978fae
Kapustin Anton <antonkapystin@hotmail.com>
2016-07-16 23:38:29 +0300

    SPRING INITIALIZR init project

---
+++ Conferencer-REST/src/main/java/me/conferencer/ConferencerRestApplication.java
@@ -0,0 +1,12 @@
+package me.conferencer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferencerRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferencerRestApplication.class, args);
+	}
+}
